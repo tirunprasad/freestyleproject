42a436f80e4e5ed1de2d944fc6055010d7d1a0a9
Tirun Prasad <tirunprasad27@gmail.com>
2022-08-15 18:10:52 +0530

    last commit

--- src/main/java/com/kenrigtechservices/FreeStyleProjectApplication.java
+++ src/main/java/com/kenrigtechservices/FreeStyleProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreeStyleProj1Application {
+public class FreeStyleProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FreeStyleProj1Application.class, args);
+		SpringApplication.run(FreeStyleProjectApplication.class, args);
 	}
 
 }
--- src/test/java/com/kenrigtechservices/FreeStyleProjectApplicationTests.java
+++ src/test/java/com/kenrigtechservices/FreeStyleProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FreeStyleProj1ApplicationTests {
+class FreeStyleProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
